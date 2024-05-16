990ee3203eb5a1eca8b74cf87bd5a5f2c7110aa9
panda_runico <becerra.jesusantonio@gmail.com>
2024-05-15 22:22:18 -0500

    feat: 🎸 callback excercises

---
+++ src/callback/index.js
@@ -0,0 +1,27 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+function calc(num1, num2, callback) {
+  return callback(num1, num2);
+};
+console.log(calc(4, 4, sum));
+
+setTimeout(() => {
+  console.log('Hola JavaScript')
+}, 5000);
+
+function greeting(name) {
+  console.log(`Hola ${name}`);
+}
+
+setTimeout(greeting, 2000, 'Jesus')
+
+function execCallback(callback) {
+  setTimeout(callback, 2000, 'Jesus')
+}
+
+function guilty(name) {
+  console.log(`${name} No entendió el ejercicio XD`)
+}
+
+execCallback(guilty)
