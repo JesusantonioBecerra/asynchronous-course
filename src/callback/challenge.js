a3dcedacb3a25a48ebfe486325716181810141ad
panda_runico <becerra.jesusantonio@gmail.com>
2024-05-15 23:34:23 -0500

    feat: 🎸 callback fetchData
    use the function fetchData get the information for product list, title,
    and cathegory . Put the command in package json

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,4 @@
-const XMLHttpRequest = require('xmlhttprequest');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 const API = 'https://api.escuelajs.co/api/v1';
 
 function fetchData(urlApi, callback) {
@@ -6,13 +6,29 @@ function fetchData(urlApi, callback) {
 
   xhttp.open('GET', urlApi, true);
   xhttp.onreadystatechange = function (event) {
-    if (xhttp.readyState === 4 && xhttp.status === 200) {
-      callback(null, JSON.parse(xhttp.responseText))
-    } else {
-      const error = new Error('Error' + urlApi)
-      return callback(error, null)
-    }
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        callback(null, JSON.parse(xhttp.responseText))
+      } else {
+        const error = new Error('Error' + urlApi)
+        return callback(error, null)
+      }
+    } 
   }
   xhttp.send();
 }
 
+fetchData(`${API}/products`, function(err, data){
+  if (err) return console.error(err);
+  // console.log(data);
+  fetchData(`${API}/products/${data[0].id}`, function(err2, data2){
+    if (err2) return console.error(err2);
+    // console.log(data2);
+  if (err) return console.error(err);
+    fetchData(`${API}/categories/${data2?.category?.id}`, function(err3, data3) {
+      if (err3) return console.error(err3);
+      // console.log(data3);
+      console.log(data[0],'->' ,data2.title, '->', data3.name);
+    });
+  });
+});
