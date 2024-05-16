611b72425b6f4a2d9f1ca3ccced759a04955c651
panda_runico <becerra.jesusantonio@gmail.com>
2024-05-15 22:41:54 -0500

    feat: 🎸 create fetchData function

--- src/callback/index.js
+++ src/callback/index.js
@@ -5,7 +5,7 @@ function calc(num1, num2, callback) {
   return callback(num1, num2);
 };
 console.log(calc(4, 4, sum));
-
+//////////////////////
 setTimeout(() => {
   console.log('Hola JavaScript')
 }, 5000);
@@ -15,7 +15,7 @@ function greeting(name) {
 }
 
 setTimeout(greeting, 2000, 'Jesus')
-
+////////////////////////////////
 function execCallback(callback) {
   setTimeout(callback, 2000, 'Jesus')
 }
